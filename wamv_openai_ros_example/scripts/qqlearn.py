60396b103525b237070157d3e6ff3e215ca0ae76
amysniffen <amy.k.sniffen.gr@dartmouth.edu>
2019-02-27 09:06:52 -0500

    added file for double q learning

---
+++ wamv_openai_ros_example/scripts/qqlearn.py
@@ -0,0 +1,29 @@
+import random
+
+class QQLearn:
+
+    def __init__(self, actions, epsilon, gamma):
+        self.q = {}
+        self.epsilon = epsilon  # exploration constant
+        self.alpha = alpha      # discount constant
+        self.gamma = gamma      # discount factor
+        self.actions = actions
+
+    def getQ(self, state, action):
+        return self.q.get((state, action), 0.0)
+
+    def chooseAction(self, state, return_q=False):
+        q = [self.getQ(state, a) for a in self.actions]
+
+        #maxQ = max(q)
+
+    def learnQ(self, state, action, reward, value):
+        '''
+        Q-learning:
+            Q(s, a) += alpha * (reward(s,a) + max(Q(s') - Q(s,a))
+        '''
+        oldv = self.q.get((state, action), None)
+        if oldv is None:
+            self.q[(state, action)] = reward
+        else:
+            self.q[(state, action)] = oldv + self.alpha * (value - oldv)
