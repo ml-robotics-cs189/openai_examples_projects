6ab87d9b327b7573ec6aaaad4401e572b06a130e
jenn738 <jenniferjain10@gmail.com>
2019-03-02 19:55:04 -0500

    params set

--- wamv_openai_ros_example/scripts/controller.py
+++ wamv_openai_ros_example/scripts/controller.py
@@ -24,15 +24,18 @@ if __name__ == '__main__':
 
     rospy.init_node('wamv_nav_twosets_buoys', anonymous=True, log_level=rospy.WARN)
 
-
-    # wait for messages where relavent
-    rospy.wait_for_message("/wamv/odom", Odometry, timeout=1.0)
-
     # Create the Gym environment
     env = gym.make('WamvNavTwoSetsBuoys-v0')
     rospy.loginfo("Gym environment done")
 
-    # somehow we need to find the goal point from above's environment
+
+    # GOAL VALUES NEED TO BE FOUND SOMEHOW (fomr environment?)
+    goal_x = rospy.get_param("~goal_x", 10)
+    goal_y = rospy.get_param("~goal_y", 10)
+
+
+    # wait for messages where relavent
+    rospy.wait_for_message("/wamv/odom", Odometry, timeout=1.0)
 
     # ROS subscribers
     rospy.Subscriber("/wamv/odom", Odometry, calculate_velcoties)
